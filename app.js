ce7554ca1b4a32a6e03ae7df0d5e258f77a45be0
Alan Vítor Paixão Almeida <vitorpaixaoa.loubi@gmail.com>
2020-09-22 15:21:23 -0300

    Rotas em desenvolvimento

--- app.js
+++ app.js
@@ -18,7 +18,37 @@ const Produto = mongoose.model("Produto");
 //EndPoints 
 
     //cadastros
-    app.post();
+    app.post("/produto",(req, res) => {
+
+        if( req.body.nome != undefined &&
+            req.body.preco != undefined &&
+            req.body.fabricante != undefined &&
+            req.body.quantidade != undefined ) {
+                
+
+                var produto = new Produto({
+                    nome: req.body.nome,
+                    fabricante: req.body.fabricante,
+                    quantidade: req.body.quantidade,
+                    preco: req.body.preco
+                });
+                produto.save().then(() => {
+                    res.statusCode = 201
+                    res.send("Item adicionado");
+                }).catch((erro)=> {
+                    if(erro){
+                        throw erro; //impedir q o sistema caia se acontecer um erro.
+                    }
+                    // Aconteceu alguma falha
+                    res.statusCode = 417
+                    res.send();
+        
+                })
+            } else{
+                res.statusCode = 406
+                res.send("Todos os dados precisam ser preenchidos.");
+            }
+    });
 
 
 app.listen(8080,() =>{
